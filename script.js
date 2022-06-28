8517dc4a56148ff632add6321b14742727b80570
Kevin Truong <ktruon11@montgomerycollege.edu>
2022-06-28 19:44:41 -0400

    Add functionality to recognize click and drag

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function addSquare() {
     SKETCHPAD.appendChild(square);
 }
 
+// Sets expected amount of columns in CSS grid
 function accomodateGrid(cols) {
     SKETCHPAD.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
 }
@@ -24,5 +25,22 @@ function clearGrid() {
     SKETCHPAD.innerHTML = '';
 }
 
+function setDivsColorable() {
+    const sketchSquares = document.querySelectorAll('.sketchpad>div');
+    sketchSquares.forEach(elem => elem.addEventListener('mouseenter', dragAndColor));
+}
+
+function setDivsNotColorable() {
+    const sketchSquares = document.querySelectorAll('.sketchpad>div');
+    sketchSquares.forEach(elem => elem.removeEventListener('mouseenter', dragAndColor));
+}
+
+function dragAndColor() {
+    console.log('color');
+}
+
 // Creates the default 16x16 grid
 createGrid();
+
+SKETCHPAD.addEventListener('mousedown', setDivsColorable);
+SKETCHPAD.addEventListener('mouseup', setDivsNotColorable);
