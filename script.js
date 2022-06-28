6a318469f96a3ac2edf2c5df0fcdc65c936c6368
Kevin Truong <ktruon11@montgomerycollege.edu>
2022-06-28 11:39:59 -0400

    Add functionality to create freeform grid

---
+++ script.js
@@ -0,0 +1,20 @@
+const SKETCHPAD = document.querySelector('.sketchpad');
+
+function createGrid(size) {
+    accomodateGrid(size);
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            addSquare();
+        }
+    }
+}
+
+function addSquare() {
+    const square = document.createElement('div');
+    SKETCHPAD.appendChild(square);
+}
+
+function accomodateGrid(cols) {
+    SKETCHPAD.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+}
